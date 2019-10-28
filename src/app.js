94d4bfcff8aa53c07ee1bba5a79a123462fc888a
KGood93 <goodreau.kath@gmail.com>
2019-10-27 19:52:27 -0500

    Update app, config and server

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const {NODE_ENV} = require('./config')
+const folderRouter = require('./folders/folder-router')
+const noteRouter = require('./notes/note-router')
 
 const app = express()
 
@@ -14,6 +16,9 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
+
+app.use('/api/folders', folderRouter)
+app.use('/api/notes', noteRouter)
 
 app.get('/', (req, res) => {
     res.send('Hello, world!')
