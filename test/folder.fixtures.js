1d1eefe9a648f61b3d4f8de0f3e78f1b3f2b6a0c
KGood93 <goodreau.kath@gmail.com>
2019-10-27 20:22:37 -0500

    Add Tests

---
+++ test/folder.fixtures.js
@@ -0,0 +1,28 @@
+function makeFoldersArray() {
+    return [
+        {
+            id: 1,
+            name: 'Folder One'
+        },
+        {
+            id: 2,
+            name: 'Folder Two'
+        },
+        {
+            id: 3,
+            name: 'Folder Three'
+        },
+        {
+            id: 4,
+            name: 'Folder Four'
+        },
+        {
+            id: 5,
+            name: 'Folder Five'
+        }
+    ];
+}
+
+module.exports = {
+    makeFoldersArray
+}
---
+++ test/notes.fixtures.js
@@ -0,0 +1,69 @@
+function makeNotesArray() {
+    return [
+        {
+            id: 1,
+            name: 'Note One',
+            content: "In Folder One",
+            folder_id: 1,
+            date_modified: '2019-08-01T16:28:32.615Z'
+        },
+        {
+            id: 2,
+            name: 'Note Two',
+            content: "In Folder One",
+            folder_id: 1,
+            date_modified: '2019-08-02T16:28:32.615Z'
+        },
+        {
+            id: 3,
+            name: 'Note Three',
+            content: "In Folder Two",
+            folder_id: 2,
+            date_modified: '2019-08-03T16:28:32.615Z'
+        },
+        {
+            id: 4,
+            name: 'Note Four',
+            content: "In Folder Three",
+            folder_id: 3,
+            date_modified: '2019-08-04T16:28:32.615Z'
+        },
+        {
+            id: 5,
+            name: 'Note Five',
+            content: "In Folder Three",
+            folder_id: 3,
+            date_modified: '2019-08-04T16:28:32.615Z'
+        },
+        {
+            id: 6,
+            name: 'Note Six',
+            content: "In Folder Four",
+            folder_id: 4,
+            date_modified: '2019-08-04T16:28:32.615Z'
+        },
+        {
+            id: 7,
+            name: 'Note Seven',
+            content: "In Folder Five",
+            folder_id: 5,
+            date_modified: '2019-08-04T16:28:32.615Z'
+        },
+        {
+            id: 8,
+            name: 'Note Eight',
+            content: "In Folder Five",
+            folder_id: 5,
+            date_modified: '2019-08-04T16:28:32.615Z'
+        },
+        {
+            id: 9,
+            name: 'Note Nine',
+            content: "In Folder Five",
+            folder_id: 5,
+            date_modified: '2019-08-04T16:28:32.615Z'
+        }
+    ]
+}
+
+module.exports = {makeNotesArray}
